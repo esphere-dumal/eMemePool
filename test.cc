3cbb868d033731acae18a51f111a866b11937e95
Lesphere <esphere@qq.com>
2022-10-05 22:45:10 +0800

    make a test, going to debug

--- test.cc
+++ test.cc
@@ -1,7 +1,11 @@
 #include <iostream>
 
 int main() {
-    int* ptr = (int*)(malloc(sizeof(int)));
+    void* ptr = malloc(sizeof(int));
+    void* intptr = (int*)ptr;
     std::cout << "ptr value is: "<< ptr << std::endl;
+    std::cout << "intptr value is: "<< intptr << std::endl;
+
+    std::cout << "(int)" << size_t(ptr) << std::endl;
     return 0;
 }
